8d20642807c8a9df8735c3006fe0c4345a7db26e
Huynh Anh Kiet <akiethuynh0904@gmail.com>
2025-12-18 13:29:31 +0700

    add new

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path' );
+// Vs7
+const { engine } = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+//add img
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('.hbs', engine(
+  {
+    // đổi tên miền sau cho ngắn lại
+    extname: '.hbs'
+  }
+));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
